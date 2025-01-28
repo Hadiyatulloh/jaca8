130579b6a208faec9658fbc734809c8b18557fbc
Hadiyatulloh <hadiyatullohfayzullayev@gmail.com>
2025-01-28 16:23:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sumDisplay = document.getElementById('sumDisplay');
 let a = prompt("1 dan 100 gacha son kiriting")
 let sum = 0;
 
-if (!isNaN(a) && a <= 100) {
+if (!isNaN(a) && a <= 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000) {
     button.addEventListener('click', () => {
         sum += Number(a);
         sumDisplay.textContent = `
